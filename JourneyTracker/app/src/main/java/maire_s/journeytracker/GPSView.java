38308076fb082f984a6a524254948be2c0e3ab93
seb776 <sebastien.maire@epitech.eu>
2016-04-26 18:26:04 +0100

    Milestone 03 & 04 (04 was part done in Milestone 01)

--- JourneyTracker/app/src/main/java/maire_s/journeytracker/GPSView.java
+++ JourneyTracker/app/src/main/java/maire_s/journeytracker/GPSView.java
@@ -6,6 +6,9 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.util.AttributeSet;
@@ -14,6 +17,11 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Switch;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class GPSView extends View {
     private float _maxSpeedDisplay;
@@ -24,17 +32,65 @@ public class GPSView extends View {
     private float _currentSpeed;
     private boolean _isTracking;
 
+    private LocationListener _locListen;
+    private LocationManager _locMgr;
+    private final int _maxSamples;
+
+    Deque<Location> _locations;
+
     public GPSView(Context context, AttributeSet attrs) {
         super(context, attrs);
         _maxSpeedDisplay = 60.0f;
         _speedDisplaySteps = 10.0f;
-        _isTracking = false;
+        _isTracking = true;
+        _maxSamples = 100;
+
+        _locations = new ArrayDeque<Location>(_maxSamples);
+        _locMgr = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        _locListen = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (_isTracking)
+                {
+                    _locations.addLast(location);
+                    if (_locations.size() >= _maxSamples)
+                    {
+                        _locations.removeFirst();
+                    }
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        SwitchTracking();
     }
 
 
     public void SwitchTracking()
     {
         _isTracking = !_isTracking;
+        if (_isTracking)
+        {
+            _locMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, _locListen);
+        }
+        else
+        {
+            _locMgr.removeUpdates(_locListen);
+        }
     }
     public boolean IsTracking()
     {
