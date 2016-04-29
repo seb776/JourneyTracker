22ed20a6d6494c987c89fdbcd8597d14882ffde4
seb776 <sebastien.maire@epitech.eu>
2016-04-29 23:28:07 +0100

    Milestone 05: App finished

--- JourneyTracker/app/src/main/java/maire_s/journeytracker/GPSView.java
+++ JourneyTracker/app/src/main/java/maire_s/journeytracker/GPSView.java
@@ -4,88 +4,100 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.CalendarContract;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.Switch;
-
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.concurrent.LinkedBlockingDeque;
+import java.util.ArrayList;
 
 public class GPSView extends View {
     private float _maxSpeedDisplay;
     private float _speedDisplaySteps;
     private int _samplesCount;
-
     private float _avgSpeed;
     private float _currentSpeed;
     private boolean _isTracking;
-
+    private ArrayList<Location> _locations;
     private LocationListener _locListen;
     private LocationManager _locMgr;
     private final int _maxSamples;
+    private float _averageSpeed;
 
-    Deque<Location> _locations;
+    private Paint _green;
+    private Paint _red;
+    private Paint _white;
+    private Paint _black;
 
-    public GPSView(Context context, AttributeSet attrs) {
+    public GPSView(final Context context, AttributeSet attrs) {
         super(context, attrs);
+        _green = new Paint();
+        _green.setColor(Color.rgb(0, 255, 0));
+        _green.setStrokeWidth(2);
+
+        _red = new Paint();
+        _red.setColor(Color.rgb(255, 0, 0));
+        _red.setStrokeWidth(2);
+
+        _white = new Paint();
+        _white.setColor(Color.rgb(255,255,255));
+        _white.setStrokeWidth(2);
+
+        _black = new Paint();
+        _black.setColor(Color.rgb(0, 0, 0));
+
         _maxSpeedDisplay = 60.0f;
         _speedDisplaySteps = 10.0f;
         _isTracking = true;
         _maxSamples = 100;
 
-        _locations = new ArrayDeque<Location>(_maxSamples);
+        _locations = new ArrayList<Location>(_maxSamples);
         _locMgr = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        final GPSView self = this; // to access the right "this" through the LocationListener
         _locListen = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
                 if (_isTracking)
                 {
-                    _locations.addLast(location);
+                    _locations.add(location);
                     if (_locations.size() >= _maxSamples)
                     {
-                        _locations.removeFirst();
+                        _locations.remove(0);
                     }
+                    _calculateAverageSpeed();
+                    self.invalidate(); // To udate visual
                 }
             }
 
             @Override
             public void onStatusChanged(String provider, int status, Bundle extras) {
-
+                //((JourneyActivity)context).setGPSStatusText(status == LocationProvider.AVAILABLE);
             }
 
             @Override
             public void onProviderEnabled(String provider) {
-
+                ((JourneyActivity)context).setGPSStatusText(true);
             }
 
             @Override
             public void onProviderDisabled(String provider) {
-
+                ((JourneyActivity)context).setGPSStatusText(false);
             }
         };
-
         SwitchTracking();
     }
-
+    public boolean GetGPSStatus()
+    {
+        return _locMgr.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
 
     public void SwitchTracking()
     {
         _isTracking = !_isTracking;
         if (_isTracking)
         {
-            _locMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, _locListen);
+            _locMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, _locListen);
         }
         else
         {
@@ -101,27 +113,56 @@ public class GPSView extends View {
     {
         canvas.translate(0, 0);
         super.onDraw(canvas);
-        Paint black = new Paint();
-        black.setColor(Color.rgb(0, 0, 0));
+        canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), _black);
 
-        Paint green = new Paint();
-        green.setColor(Color.rgb(0, 255, 0));
-        green.setStrokeWidth(2);
-        canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), black);
         float verticalStepsSpeed = canvas.getHeight() / (_maxSpeedDisplay / _speedDisplaySteps);
         float speedLine = 0.0f;
         while (speedLine < canvas.getHeight())
         {
-            canvas.drawLine(0.0f, speedLine, canvas.getWidth(), speedLine, green);
+            canvas.drawLine(0.0f, speedLine, canvas.getWidth(), speedLine, _white);
             speedLine += verticalStepsSpeed;
+        }
+        final float averageSpeedLine = canvas.getHeight() - ( (canvas.getHeight() / _maxSpeedDisplay) * _averageSpeed);
+
+        canvas.drawLine(0.0f, averageSpeedLine, canvas.getWidth(), averageSpeedLine, _red);
+
+        float heightMult = canvas.getHeight() / _maxSpeedDisplay;
+        int xSpace = canvas.getWidth() / (_maxSamples - 1);
+        int xIt = 0;
+        for (int i = 0; i < _locations.size(); ++i)
+        {
+            int nextIndex = Math.min(i + 1, _locations.size() - 1);
+            int xStop = nextIndex * xSpace;
+            int yStart = canvas.getHeight() - (int)(heightMult * _locations.get(i).getSpeed());
+            int yStop = canvas.getHeight () - (int)(heightMult * _locations.get(nextIndex).getSpeed());
+            canvas.drawLine(xIt, yStart, xStop, yStop, _green);
+            xIt += xSpace;
         }
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        setMeasuredDimension(widthMeasureSpec, heightMeasureSpec - 200);
+        final int size = Math.min(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(size, size - 200);
     }
 
-
+    public float GetAverageSpeed()
+    {
+        return _averageSpeed;
+    }
+    public float GetCurrentSpeed()
+    {
+        if (_locations.size() > 0)
+            return _locations.get(_locations.size() - 1).getSpeed();
+        return 0.0f;
+    }
+    private void _calculateAverageSpeed()
+    {
+        float avg = 0.0f;
+        for (Location l : _locations)
+            avg += l.getSpeed();
+        avg /= (float)_locations.size();
+        _averageSpeed = avg;
+    }
 }
--- JourneyTracker/app/src/main/java/maire_s/journeytracker/JourneyActivity.java
+++ JourneyTracker/app/src/main/java/maire_s/journeytracker/JourneyActivity.java
@@ -1,19 +1,29 @@
 package maire_s.journeytracker;
 
+
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class JourneyActivity extends AppCompatActivity {
     private boolean _isTracking;
     private GPSView _gpsTracker;
     private Button _btnTrack;
+    private TextView _gpsActive;
+    private TextView _curSpeed;
+    private TextView _avgSpeed;
+    private TextView _overallTime;
+    private Timer _timer;
+    private int  _seconds;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,18 +40,75 @@ public class JourneyActivity extends AppCompatActivity {
             }
         });
 
+        _timer = null;
+
+        _gpsActive = (TextView)findViewById(R.id.gpsActive);
+        _curSpeed = (TextView)findViewById(R.id.curSpeed);
+        _avgSpeed = (TextView)findViewById(R.id.avgSpeed);
+        _overallTime = (TextView)findViewById(R.id.overallTime);
+
+        _seconds = 0;
+        setGPSStatusText(_gpsTracker.GetGPSStatus());
+        setCurrentSpeedText(false, 0.0f);
+        setAverageSpeedText(0.0f);
+        setOverallTimeText(_seconds);
         _applyTrackState();
 
+    }
 
+    public void setGPSStatusText(boolean status)
+    {
+        _gpsActive.setText("GPS " + (status ? "Active" : "Inactive"));
+        if (!status)
+        {
+            setCurrentSpeedText(false, 0.0f);
+        }
+    }
+
+    private void setCurrentSpeedText(boolean known, float value)
+    {
+        _curSpeed.setText("Current speed: " + (known ? Float.toString(value) + "km/h" : "N/A"));
+    }
+
+    private void setAverageSpeedText(float value)
+    {
+        _avgSpeed.setText("Average speed: " + Float.toString(value) + "km/h");
+    }
+
+    private void setOverallTimeText(int seconds)
+    {
+        _overallTime.setText("Overall time: " + Integer.toString(seconds) + "s");
     }
 
     private void _applyTrackState()
     {
         if (_gpsTracker.IsTracking()) {
             _btnTrack.setText("Stop tracking");
+            _timer = new Timer();
+            TimerTask timerTask = new TimerTask() {
+                @Override
+                public void run() {
+                    ++_seconds;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            setOverallTimeText(_seconds);
+                            setAverageSpeedText(_gpsTracker.GetAverageSpeed());
+                            setCurrentSpeedText(true, _gpsTracker.GetCurrentSpeed());
+                        }
+                    });
+                }
+            };
+            _timer.scheduleAtFixedRate(timerTask, 0, 1000);
         }
         else {
             _btnTrack.setText("Start Tracking");
+            setCurrentSpeedText(false, 0.0f);
+            if (_timer != null) {
+                _timer.cancel();
+                _timer.purge();
+                _timer = null;
+            }
         }
     }
 
